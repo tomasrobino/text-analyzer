6fa1469bf09b118bfc164927a3e5bd58937f54bb
Tomas <tomaserobino@gmail.com>
2025-02-04 12:15:20 +0100

    changed code partially to work with linked list

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct node_t {
-    char* data;
+    int amount;
+    char* word;
     struct node_t *next;
 } node;
 
@@ -21,6 +23,11 @@ void readFile(char name[]) {
     long byteAmount = position;
     printf("byteAmount: %ld\n", byteAmount);
     rewind(file);
+    node* head = malloc(sizeof(node));
+    head->word = NULL;
+    head->next = NULL;
+    head->amount = 0;
+    node* currentNode = head;
     long charAmount = 0;
     long wordLength = 0;
     long wordTotal = 0;
@@ -32,41 +39,42 @@ void readFile(char name[]) {
             charAmount++;
         } else {
             if (wordLength != 0) {
+                char* word = malloc(wordLength+1);
+                fread(word, 1, wordLength+1, file);
+                word[wordLength] = '\0';
+                int end = 0;
+                node* prevNode = NULL;
+                while (currentNode != NULL && !end) {
+                    end = strcmp(word, currentNode->word);
+                    if (end) {
+                        prevNode = currentNode;
+                        currentNode = currentNode->next;
+                    }
+                }
+                //Word not found in list
+                if (currentNode == NULL) {
+                    node* newNode = malloc(sizeof(node));
+                    newNode->amount = 1;
+                    newNode->word = word;
+                    newNode->next = NULL;
+                    //prevNode should never be NULL
+                    if (prevNode != NULL) {
+                        prevNode->next = newNode;
+                    } else exit(99);
+                } else {
+                    //Word found
+                    currentNode->amount++;
+                }
+                currentNode = head;
                 wordLength = 0;
                 wordTotal++;
             }
         }
-        ftell(file);
     }
+    free(head);
+    head = currentNode->next;
+    currentNode = currentNode->next;
     rewind(file);
-
-    //Creating wordArray, allocating all words
-    char** wordArray = malloc(sizeof(char*)*wordTotal);
-    long wordCounter = 0;
-    wordLength = 0;
-
-    for (int i = 0; i < byteAmount; i++) {
-        const int currentChar = fgetc(file);
-        //ASCII 32 is space, 127 is DEL, 160 is NBSP
-        if (isalpha(currentChar)) {
-            wordLength++;
-        } else {
-            if (wordLength != 0) {
-                wordArray[wordCounter] = malloc(wordLength+1);
-                position = ftell(file);
-                //-1 is EOF, correct position
-                if (currentChar == -1) {
-                    position++;
-                }
-                rewind(file);
-                fseek(file, position-wordLength-1, SEEK_CUR);
-                fread(wordArray[wordCounter], 1, wordLength+1, file);
-                wordArray[wordCounter][wordLength] = '\0';
-                wordLength = 0;
-                wordCounter++;
-            }
-        }
-    }
     puts("");
 
     printf("charAmount (without spaces, new lines, etc.): %ld\n", charAmount);
@@ -74,8 +82,15 @@ void readFile(char name[]) {
     printf("words:\n");
 
     for (int i = 0; i<wordTotal;i++) {
-        printf("%s ", wordArray[i]);
+        if (currentNode != NULL) {
+            printf("word: %s, amount: %d", currentNode->word, currentNode->amount);
+            currentNode = currentNode->next;
+        }
     }
-    free(wordArray);
+    while (head != NULL) {
+        node* auxNode = head;
+        head = head->next;
+        free(auxNode);
+    }
     fclose(file);
 }
